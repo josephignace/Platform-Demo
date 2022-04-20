1b908c62743e8c0bbb9902c43b45194631823aa5
Jose Ignacio Cuitun Coronado <61969236+josephignace@users.noreply.github.com>
2022-04-20 10:34:53 +0100

    Create 1-plot-stppop.R

---
+++ analysis/1-plot-stppop.R
@@ -0,0 +1,46 @@
+## Import libraries
+library('tidyverse')
+
+## import data
+df_input <- read_csv(
+    here::here("outputs", "cohorts", "input_1_stppop.csv"),
+    col_types = cols(
+        patient_id = col_integer(),
+        stp = col_character()
+    )
+)
+
+df_stppop = df_input %>% count(stp, name = 'registered')
+
+## Bar chart
+plot_stppop_bar <- df_stppop %>%
+    mutate(
+        name = forcats::fct_reorder(stp, registered, median, .desc = FALSE)
+    ) %>%
+    ggplot() + geom_col(aes(x=registered/1000000, y =name, fill=registered), colour = 'black') +
+               scale_fill_gradient(limits = c(0, NA), low = "white", high = "blue", guide = FALSE) +
+               labs(
+                   title = "TPP-registered patients per STP",
+                   subtitle = "as at 1 January 2020",
+                   y = NULL,
+                   x = "Registered patients\n(million)",
+                   fill = NULL) =
+                theme_minimal() + 
+                theme(panel.grid.major.y = element_blank(),
+                      panel.grid.minor.y = elemenet_blank(),
+                      plot.title.position = "plot",
+                      plot.caption.position = "plot"
+                    )
+
+plot_stppop_bar
+
+## Save plot
+dir.create(here::here("output", "plots")) #  Put the output in a subfolder, it needs to be created
+
+ggsave(
+    plot = plot_stppop_bar,
+    filename = "plots_stppop_bar.png", path = here::here("outputs", "plots"),
+    units = "cm",
+    height = 15,
+    width = 15
+)
